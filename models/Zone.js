9ac31c4084fabf07ad63166a1a3baf44148e70c7
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-09-02 01:12:23 +0700

    create(schema): created schema for zone

---
+++ models/Zone.js
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose";
+
+const ZoneSchema = new Schema(
+  {
+    zoneName: { type: String, unique: true, required: true },
+    postalCodes: {
+      type: [String],
+      default: [],
+      required: true,
+      validate: {
+        validator: (arr) => new Set(arr).size === arr.length,
+        message: "Postal codes must be unique within a zone",
+      },
+    },
+    shippingFee: { type: Number, min: 0, required: true },
+  },
+  { timestamps: true }
+);
+
+ZoneSchema.pre("save", async function(next) {
+  const overlapping = await Zone.findOne({
+    postalCodes: { $in: this.postalCodes }
+  });
+  if (overlapping) {
+    return next(new Error("Some postal codes already exist in another zone"));
+  }
+  next();
+});
+
+export const Zone = model("Zone", ZoneSchema);
