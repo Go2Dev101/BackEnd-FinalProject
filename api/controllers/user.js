3c5e95df610950608930442fe8a4b949619b8e6a
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-09-16 15:27:45 +0700

    fix(route logout): edit clear cookie

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -160,10 +160,11 @@ export const getUserProfile = async (req, res, next) => {
 
 // Logout user
 export const logoutUser = (req, res, next) => {
+  const isProd = process.env.NODE_ENV === "production";
   res.clearCookie("accessToken", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "None",
+    secure: isProd,
+    sameSite: isProd ? "none" : "lax",
   });
   res.status(200).json({ message: "Logged out successfully" });
 };
