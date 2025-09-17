3b1153d01ec5d3eb4a2806a28c85b6aab58cd28e
supasin <dev.snakphu7@gmail.com>
2025-09-17 13:44:28 +0700

    create router orderHistory

--- api/router/order.js
+++ api/router/order.js
@@ -4,12 +4,14 @@ import {
   getAllOrders,
   getOrderById,
   updateOrderStatusById,
+  getOrderHistory,
 } from "../controllers/order.js";
 import { authUser } from "../../middleware/authUser.js";
 
 const router = express.Router();
 
 router.get("/", getAllOrders);
+router.get("/orderHistory", authUser,getOrderHistory);
 router.post("/", authUser, createOrder);
 router.get("/:orderId", authUser, getOrderById);
 router.put("/:orderId", authUser, updateOrderStatusById);
